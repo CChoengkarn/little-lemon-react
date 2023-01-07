4088a1f453141cbd2a17fa307925c4d7d5600bed
CChoengkarn <111892520+CChoengkarn@users.noreply.github.com>
2023-01-06 16:01:21 -0800

    9th commit
    made overall changes to the layout

--- src/Nav.js
+++ src/Nav.js
@@ -2,14 +2,16 @@ import React from 'react';
 
 function Nav() {
     return(
-   <ul className="navbar">
-    <li><a href="#home">Home</a></li>
-    <li><a href="#about">About</a></li>
-    <li><a href="#menu">Menu</a></li>
-    <li><a href="#reservations">Reservations</a></li>
-    <li><a href="#order online">Order Online</a></li>
-    <li><a href="#login">Login</a></li>
+   <nav className="Nav">
+    <ul>
+        <li><a href="#login">login</a></li>
+        <li><a href="#order online">Order Online</a></li>
+        <li><a href="#reservation">Reservation</a></li>
+        <li><a href="#menu">Menu</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#home">Home</a></li>
     </ul>
+    </nav>
 )};
 
 export default Nav;
