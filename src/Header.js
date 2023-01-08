b262555c2300ca4c64a240643ea694da35e44908
CChoengkarn <111892520+CChoengkarn@users.noreply.github.com>
2023-01-07 16:26:06 -0800

    10th commit
    modified the "about" section and added photos

--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 function Header() {
     return (
-    <img src="/images/photo1.png" alt="little lemon logo"></img>
+    <img src="../images/photo1.png" alt="little lemon logo"></img>
 )};
 
-
 export default Header;
+
+
