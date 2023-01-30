6a8bff89528c369c37181e47b849b8ff091aa8f6
CChoengkarn <111892520+CChoengkarn@users.noreply.github.com>
2023-01-30 13:19:15 -0800

    24th commit
    made changes to files

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,146 +1,82 @@
 import React, { useState } from "react";
 
-const BookingForm = ({ availableTimes, setAvailableTimes, submitForm }) => {
-        const initDate = new Date();
-        initDate.setDate(initDate.getDate() +1);
-        const YYYY = initDate.getFullYear();
-        const MM=
-        initDate.getMonth() < 10
-        ? "0" + (initDate.getMonth() +1 )
-        : initDate.getMonth() + 1;
-        const DD=
-        initDate.getDate() < 10 ? "0" + initDate.getDate() : initDate.getDate();
+const BookingForm = () => {
+  const [name, setName] = useState('');
+  const [availableTimes, setAvailableTimes] = useState('17:00');
+  const [date, setDate] = useState('');
+  const [email, setEmail] = useState('');
+  const [guest, setGuest] = useState('');
+  const [occasion, setOccasion] = useState('');
+  console.log("Date" , date);
 
-        const [name, setName] = useState();
-        const [email, setEmail] = useState();
-        const [date, setDate] = useState(YYYY + "-" + MM + "-" + DD);
-        const [time, setTime] = useState("17:00");
-        const [guests, setGuests] = useState(2);
-        const [occassion, setOccassion] = useState("none");
+  const handleSubmit = (e) => {e.preventDefault()
+    const answer=window.confirm("Reservation has been submitted") 
+    }
+  
 
-        function isValidDate(dateString) {
-          const yyyymmdd = dateString.split("-");
-          const dateObj = new Date(
-            parseInt(yyyymmdd[0]),
-            parseInt(yyyymmdd[1]) -1,
-            parseInt(yyyymmdd[2])
-          );
-          if (dateObj <= new Date()) return false;
-          return true;
-        }
+  return (
+    <div className="formInput">
+      <h2>Booking a Reservation</h2>
+      <form onSubmit={handleSubmit}>
+      <label>Name</label>
+      <input
+        type="text"
+        value={name}
+        required
+        onChange={(e) => setName(e.target.value)}
 
-        function getDateObject(dateString) {
-          const yyyymmdd = dateString.split("-");
-          const dateObj = new Date(
-            parseInt(yyyymmdd[0]),
-            parseInt(yyyymmdd[1]) -1,
-            parseInt(yyyymmdd[2])
-          );
-          return dateObj;
-        }
+      />
+      <label>Time</label>
+      <select
+        value={availableTimes}
+        onChange={(e) => setAvailableTimes(e.target.value)}
+      >
+      <option value="17:00">17:00</option>
+      <option value="18:00">18:00</option>
+      <option value="19:00">19:00</option>
+      <option value="20:00">20:00</option>
+      <option value="21:00">22:00</option>
+      <option value="22:00">22:00</option>
+      </select>
 
-        function handleDateChange(e) {
-          if (!isValidDate(e.target.value)) {
-            alert('Sorry, this day/time is not available for a reservation')
-            return;
-          }
-          const dateObject = getDateObject(e.target.value);
-          setDate(e.target.value)
-          setAvailableTimes({ setBookingDate: dateObject });
-        }
-
-        function handleSubmit(e) {
-          e.preventDefault();
-          const reservation = {
-            name: name,
-            email: email,
-            date: date,
-            time: time, 
-            guests: guests,
-            occassion: occassion,
-          };
-          submitForm(reservation);
-        }
-
-            return(
-              <form onSubmit={handleSubmit}>
-                <h1>Reserve a Table</h1>
-                <div className="form-group">
-                  <label htmlFor="name">Name</label>
-                  <input
-                  type="text"
-                  value={name}
-                  minlength="1"
-                  maxlength="50"
-                  onChange={(e) => setName(e.target.value)}
-                  required
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="email">Email Address</label>
-                  <input
-                  type="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="Date">Date</label>
-                  <input
-                  type="date"
-                  name="date"
-                  value={date}
-                  onChange={handleDateChange}
-                  required
-                  />
-                </div>
-                <div className="form-group">
-               <label htmlFor="time">Time</label>
-               <select
-                name="time"
-              value={time}
-              onChange={(e) => setTime(e.target.value)}
-              required
-              >
-             {availableTimes.times.map((time, index) => {
-                return (
-                <option value={time} key={index}>
-                {time}
-              </option>
-            );
-          })}
-            </select>
-         </div>
-            <div className="form-group">
-            <label htmlFor="guests">Guests</label>
-             <input
-              type="number"
-              min="1"
-              max="10"
-              value={guests}
-              onChange={(e) => setGuests(e.target.value)}
-              required
-         />
-        </div>
-       <div className="form-group">
-        <label htmlFor="occassion">Occassion</label>
-        <select
-          name="occassion"
-          value={occassion}
-          onChange={(e) => setOccassion(e.target.value)}
-        >
-          <option value="none">None</option>
-          <option value="Birthday">Birthday</option>
-          <option value="Anniversary">Anniversary</option>
-          <option value="Date">Date</option>
-          <option value="Graduation">Graduation</option>
-        </select>
+      <label>Date</label>
+      <input
+        type="date"
+        required
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+      />
+      <label>Email</label>
+      <input
+        type="email"
+        required
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        />
+      <label>Number Of Guests</label>
+      <input
+        type="number"
+        min="1"
+        max="10"
+        value={guest}
+        required
+        onChange={(e) => setGuest(e.target.value)}
+      />
+      <label>Occasion</label>
+      <select
+      type="occasion"
+      value={occasion}
+      >
+       <option value="birthday">Birthday</option>
+       <option value="Anniversary">Anniversary</option>
+       <option value="other">Other</option>
+       </select>
+      <button type="submit">Submit
+      </button>
+      </form>
       </div>
-      <input type="submit" value="Confirm Reservation" />
-    </form>
-  );
-};
+  )
+  }
 
 export default BookingForm;
 
